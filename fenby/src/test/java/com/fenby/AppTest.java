462e6469ec1f81d2d71de35665c4f3b4e07833db
chenjianhua <c2290910211@163.com>
2016-11-19 11:15:08 +0800

    fenby

---
+++ fenby/src/test/java/com/fenby/AppTest.java
@@ -0,0 +1,12 @@
+package com.fenby;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class AppTest {
+  @Test
+  public void mainTest() {
+    App app = new App();
+    String[] args = {};
+    app.main(args);
+  }
+}
