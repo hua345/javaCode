87d742beb86c80507dd8f0631f019298ad158558
chenjianhua <c2290910211@163.com>
2017-05-16 01:49:38 +0800

    :apple:

--- angularjs/app/index.js
+++ angularjs/app/index.js
@@ -1,7 +1,9 @@
 const angular = require('angular');//引入angular
 const ngModule = angular.module('app',[]);//定义一个angular模块
 const _ = require('lodash');
-
+const $ = require("jquery");
+require ("bootstrap/dist/css/bootstrap.css");
+require ("bootstrap/dist/js/bootstrap.js");
 function component () {
   var element = document.createElement('div');
 
--- angularjs/webpack.config.js
+++ angularjs/webpack.config.js
@@ -7,12 +7,32 @@ module.exports = {
      path: path.resolve(__dirname, 'dist')
   },
   module: {
-    loaders:[
-      {
-        test: /\.js[x]?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader?presets[]=es2015'
-      },
-    ]
+    rules: [
+    {
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['env']
+        }
+      }
+    },
+    {
+          test: require.resolve('jquery'),
+          use: [{
+              loader: 'expose-loader',
+              options: 'jQuery'
+          },{
+              loader: 'expose-loader',
+              options: '$'
+          }]
+    },
+    { test: /\.css$/, loader: 'style-loader!css-loader' },
+    { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader" },
+    { test: /\.(woff|woff2)$/, loader:"url-loader?prefix=font/&limit=5000" },
+    { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/octet-stream" },
+    { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=image/svg+xml" }
+  ]
   }
 }
